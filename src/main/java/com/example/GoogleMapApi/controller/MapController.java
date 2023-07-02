253268e839040f6683027fc5e0847d92dd329c26
Ansh Gangwar <122267775+Ansh201@users.noreply.github.com>
2023-07-02 20:33:16 +0530

    view code

---
+++ src/main/java/com/example/GoogleMapApi/controller/MapController.java
@@ -0,0 +1,15 @@
+package com.example.GoogleMapApi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+
+	 @GetMapping("/showMap")
+	    public String index() {
+	        return "index";
+
+}
+}
